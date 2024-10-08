21b7d263824539c41951b7fd1d668ce0e557a58f
datoqarqusovi <qarqusovi54@gmail.com>
2024-10-08 19:05:14 +0400

    first commmit'

---
+++ day 0/day0/lesson1.py
@@ -0,0 +1,38 @@
+from turtle import *
+#saxls vaketebt
+speed(30)
+width(9)
+color("yellow")
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+
+
+forward(200)
+left(90)
+
+#kedlebi dasrulda
+#exla karebi
+
+forward(70)
+color("green")
+left(90)
+forward(120)
+right(90)
+forward(90)
+right(83)
+forward(120)
+penup()
+goto(200,200)
+pendown()
+color("black")
+right(140)
+forward(200)
+left(117)
+forward(185)
+exitonclick()
+
+
