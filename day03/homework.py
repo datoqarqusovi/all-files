9c52a4a101175e7d59bd6f8eafa5967089137972
datoqarqusovi <datoqarqusovi@gmail.com>
2024-11-14 00:05:58 +0400

    first commit

---
+++ day03/homework.py
@@ -0,0 +1,31 @@
+score=(65)
+print(score)
+
+helth=(554)
+print(helth)
+
+
+attack_points =(10)
+difence_points= (40)
+print(attack_points, difence_points)
+
+math_score=(10)
+english_score=(12)
+science_score=(23)
+chemistry_score=(32)
+history_score=(45)
+
+print(math_score)
+print(english_score)
+print(science_score)
+print(chemistry_score)
+print(history_score)
+
+
+
+
+
+name=("სიგიჟეა")
+username=("sasacilo")
+age=("nini")
+print(name, username, age,)
